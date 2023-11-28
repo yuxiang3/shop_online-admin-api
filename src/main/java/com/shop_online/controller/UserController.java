538dea6601ca1a56337000ab3d6002dcd8643a31
yuxiang <565671781@qq.com>
2023-11-28 13:14:06 +0800

    分页&用户信息查询

--- src/main/java/com/shop_online/controller/UserController.java
+++ src/main/java/com/shop_online/controller/UserController.java
@@ -1,9 +1,21 @@
 package com.shop_online.controller;
 
+import com.shop_online.common.result.PageResult;
+import com.shop_online.common.result.Result;
+import com.shop_online.service.UserService;
+import com.shop_online.vo.UserVO;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.shop_online.common.model.Query;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 
 /**
@@ -19,5 +31,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("user")
 @AllArgsConstructor
 public class UserController {
-    
+    private final UserService userService;
+    @PostMapping("page")
+    @ApiOperation("分页")
+    public Result<PageResult<UserVO>> page(@RequestBody @Valid Query query){
+        PageResult<UserVO> result=userService.getPage(query);
+        return Result.ok(result);
+    }
+
+    @PostMapping("export")
+    @ApiOperation("导出用户信息")
+    public Result<String> export(@RequestBody Query query, HttpServletResponse response){
+      userService.exportUserInfo(query,response);
+      return Result.ok();
+    }
 }
---
+++ src/main/java/com/shop_online/service/UserService.java
@@ -0,0 +1,30 @@
+package com.shop_online.service;
+
+import com.shop_online.common.result.PageResult;
+import com.shop_online.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.shop_online.vo.UserVO;
+
+import com.shop_online.common.model.Query;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author ycshang
+ * @since 2023-11-28
+ */
+public interface UserService extends IService<User> {
+    PageResult<UserVO> getPage(Query query);
+    /**
+     * 导出用户记录
+     *
+     * @param query
+     * @param response
+     */
+    void exportUserInfo(Query query, HttpServletResponse response);
+
+}
